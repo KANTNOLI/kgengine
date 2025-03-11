fe915d88a6dfeb4e5c6ef060f13b1c5eb7c684cf
KANTNOLI <zahargysev02@gmail.com>
2025-03-11 17:32:54 +0300

    v0.6.1 fix

--- Engine/OtherScripts/CreateAnimation.ts
+++ Engine/OtherScripts/CreateAnimation.ts
@@ -6,27 +6,27 @@ const CreateAnimation = (
   posStart: PositionObject3D = { x: 0, y: 0, z: 0 },
   posEnd: PositionObject3D = { x: 2, y: 1, z: 3 },
   callback: (progress: number) => any = () => "",
-  speed: number = 0.016
+  time: number = 1
 ) => {
   // 0.016 default SPEED
 
   //      1
   // -------------
-  // 60FPS * SPEED
+  // 60FPS * 1xSPEED
+  const cycle = 8.34 * time;
 
   let progress = 0;
-  let step = speed;
+  let step = 0.016;
 
   const move = setInterval(() => {
     progress += step;
     object.position.lerpVectors(posStart, posEnd, progress);
 
     callback(progress);
-    console.log(1);
     if (progress >= 1) {
       clearInterval(move);
     }
-  }, 16);
+  }, cycle);
 };
 
 export default CreateAnimation;
