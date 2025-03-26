21904e0e06468fdec98a5ca05a87ddf744133ddd
KANTNOLI <zahargysev02@gmail.com>
2025-03-26 12:56:39 +0300

    v0.11.8

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import * as Cameras from "./imports/cameras.js";
-import * as Engine from "./imports/engine.js";
-import * as Lighting from "./imports/lighting.js";
+import * as Cameras from "./cameras.js";
+import * as Engine from "./engine.js";
+import * as Lighting from "./lighting.js";
 import * as Objects from "./objects.js";
-import * as OtherScripts from "./imports/otherScripts.js";
+import * as OtherScripts from "./otherScripts.js";
 import * as Actions from "./playerActions.js";
 import * as Shaders from "./shaders.js";
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import path from "path";
 
 export default defineConfig({
   base: "/kgengine/",
-
   resolve: {
     alias: {
       KGEngine: path.resolve(__dirname, "./"),
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["three"],
+          kgengine: [".index.ts"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
 });
