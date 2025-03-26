222894fd4f73fae2875e4961bb90828d925a6e78
KANTNOLI <zahargysev02@gmail.com>
2025-03-26 13:51:08 +0300

    v0.11.14

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,4 @@
 import { defineConfig } from "vite";
-import path from "path";
-
 export default defineConfig({
   base: "/kgengine/",
-  resolve: {
-    alias: {
-      KGEngine: path.resolve(__dirname, "./"),
-    },
-  },
 });
