41b5fb8d0a7f25fdc4c5c6fc24394964d4c772a8
KANTNOLI <zahargysev02@gmail.com>
2025-03-26 13:44:04 +0300

    v0.11.13

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/kgengine/",
   resolve: {
     alias: {
-      KGEngine: path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ["three/examples/jsm/libs/chevrotain.module.min.js"], 
+      KGEngine: path.resolve(__dirname, "./"),
     },
   },
 });
