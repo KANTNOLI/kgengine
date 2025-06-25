3411bf59ed3ee278e6676f5ef601987f616245b9
KANTNOLI <zahargysev02@gmail.com>
2025-06-25 07:29:52 +0300

    v0.13.5

--- Engine/OtherScripts/RunAnimationMove.ts
+++ Engine/OtherScripts/RunAnimationMove.ts
@@ -8,40 +8,31 @@ export const AnimationMove = (
   posEnd: PositionObject3D = { x: 2, y: 1, z: 3 },
   callbackEvenly: (progress: number) => any = () => "",
   callbackClosed: (state: boolean) => any = () => "",
-  time: number = 1,
-  speed: number = 1
+  time: number = 1, // в секундах
+  speed: number = 60 // кадров в секунду
 ) => {
-  // 0.016 default SPEED
-
-  //      1
-  // -------------
-  // 60FPS * 1xSPEED
-  const cycle = 16.67 / speed;
-
-  let progress = 0;
-  const step = 0.01 / time;
+  const totalFrames = time * speed;
+  let frame = 0;
 
   const targetPosition = new THREE.Vector3(posEnd.x, posEnd.y, posEnd.z);
-  const epsilon = 0.01; // Допустимая погрешность
+  const epsilon = 0.01;
+
+  const interval = 1000 / speed; // миллисекунды между кадрами
 
   const move = setInterval(() => {
-    progress += step;
-    progress = Math.min(progress, 1);
+    frame++;
+    const progress = Math.min(frame / totalFrames, 1);
 
     object.position.lerpVectors(posStart, posEnd, progress);
 
     callbackEvenly(progress);
 
-    if (
-      progress * 19 >= 1 ||
-      object.position.distanceTo(targetPosition) < epsilon
-    ) {
-      callbackClosed(true);
+    if (progress >= 1 || object.position.distanceTo(targetPosition) < epsilon) {
       object.position.copy(targetPosition);
+      callbackClosed(true);
       clearInterval(move);
-      return true;
     }
-  }, cycle);
+  }, interval);
 };
 
 export default AnimationMove;
