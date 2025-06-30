74ea88678f44b20b711cc11311d1462d8ae6a9e9
KANTNOLI <zahargysev02@gmail.com>
2025-06-30 04:41:57 +0300

    v0.14.3

--- Engine/Shaders/Snippets/CuttingCustomBox.ts
+++ Engine/Shaders/Snippets/CuttingCustomBox.ts
@@ -16,13 +16,10 @@ export interface CustomCube {
   endZ: PositionObject3D;
 }
 
-const CuttingCustomBox = (
-  Figure: CustomCube,
-  tempCOLOR = 0xffffff
-): THREE.ShaderMaterial => {
+const CuttingCustomBox = (Figure: CustomCube): THREE.ShaderMaterial => {
   let useTexture = false;
   let tex: THREE.Texture | null = null;
-  let colorValue = new THREE.Color(tempCOLOR);
+  let colorValue = new THREE.Color(0xffffff);
   let opacityValue = 1;
 
   if (Figure.texture) {
@@ -31,26 +28,28 @@ const CuttingCustomBox = (
       tex = Figure.texture as THREE.Texture;
     } else if ((Figure.texture as THREE.Material).isMaterial) {
       const mat = Figure.texture as THREE.Material & any;
-      if (mat.map && (mat.map as THREE.Texture).isTexture) {
+      if (mat.map?.isTexture) {
         useTexture = true;
-        tex = mat.map as THREE.Texture;
-      } else if (mat.color && mat.color instanceof THREE.Color) {
+        tex = mat.map;
+      } else if (mat.color instanceof THREE.Color) {
         colorValue = mat.color;
       }
-      opacityValue = mat.opacity !== undefined ? mat.opacity : 0.5;
+      if (typeof mat.opacity === "number") {
+        opacityValue = mat.opacity;
+      }
     } else if (Figure.texture instanceof THREE.Color) {
       colorValue = Figure.texture;
     }
   }
 
-  const uniforms: Record<string, any> = {
+  const uniforms = {
     u_CoordLT: { value: Figure.CoordLT },
     u_CoordLB: { value: Figure.CoordLB },
     u_CoordRT: { value: Figure.CoordRT },
     u_CoordRB: { value: Figure.CoordRB },
     u_startZ: { value: Figure.startZ },
-    positionWorld: { value: Figure.positionWorld },
     u_endZ: { value: Figure.endZ },
+    u_posWorld: { value: Figure.positionWorld },
 
     u_useTexture: { value: useTexture },
     u_texture: { value: tex || new THREE.Texture() },
@@ -66,8 +65,7 @@ const CuttingCustomBox = (
     uniform mat4 u_modelMatrix;
     void main() {
       vUv = uv;
-      vec4 worldPosition = u_modelMatrix * vec4(position, 1.0);
-      vWorldPosition = worldPosition.xyz;
+      vWorldPosition = (u_modelMatrix * vec4(position, 1.0)).xyz;
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
   `;
@@ -80,8 +78,8 @@ const CuttingCustomBox = (
     uniform vec3 u_CoordRT;
     uniform vec3 u_CoordRB;
     uniform vec3 u_startZ;
-    uniform vec3 positionWorld;
     uniform vec3 u_endZ;
+    uniform vec3 u_posWorld;
 
     uniform bool u_useTexture;
     uniform sampler2D u_texture;
@@ -93,43 +91,36 @@ const CuttingCustomBox = (
 
     void main() {
       vec4 baseColor = u_useTexture ? texture2D(u_texture, vUv) : vec4(u_color, u_opacity);
-      vec3 materialColor = baseColor.rgb;
-      if (materialColor == vec3(0.0)) {
-        materialColor = vec3(1.0);
-      }
 
-      float dimmingFactor = 0.6;
-       materialColor *= dimmingFactor;
-
-      // Clipping по Z
+      // ВЫРЕЗКА по Z
       if (u_startZ.z > u_endZ.z) {
         if (vWorldPosition.z > u_startZ.z || vWorldPosition.z < u_endZ.z) {
-          gl_FragColor = vec4(materialColor, baseColor.a);
+          gl_FragColor = vec4(baseColor.rgb, baseColor.a);
           return;
         }
       } else {
         if (vWorldPosition.z < u_startZ.z || vWorldPosition.z > u_endZ.z) {
-          gl_FragColor = vec4(materialColor, baseColor.a);
+          gl_FragColor = vec4(baseColor.rgb, baseColor.a);
           return;
         }
       }
 
       // Discard-низ
-      float zDiff = vWorldPosition.z - positionWorld.z;
+      float zDiff = vWorldPosition.z - u_posWorld.z;
       float coeffRB = -zDiff / u_CoordRB.z;
       float coeffLB = -zDiff / u_CoordLB.z;
       float coeffRT = -zDiff / u_CoordRT.z;
 
       if (
-        -vWorldPosition.x + (u_startZ.x * 0.5) + positionWorld.x >= u_CoordRB.x * coeffRB &&
-        -vWorldPosition.x - (u_startZ.x * 0.5) + positionWorld.x <= u_CoordLB.x * coeffLB &&
-        -vWorldPosition.y - (u_startZ.y * 0.5) + positionWorld.y <= u_CoordRB.y * coeffRB &&
-        -vWorldPosition.y + (u_startZ.y * 0.5) + positionWorld.y >= u_CoordRT.y * coeffRT
+        -vWorldPosition.x + (u_startZ.x * 0.5) + u_posWorld.x >= u_CoordRB.x * coeffRB &&
+        -vWorldPosition.x - (u_startZ.x * 0.5) + u_posWorld.x <= u_CoordLB.x * coeffLB &&
+        -vWorldPosition.y - (u_startZ.y * 0.5) + u_posWorld.y <= u_CoordRB.y * coeffRB &&
+        -vWorldPosition.y + (u_startZ.y * 0.5) + u_posWorld.y >= u_CoordRT.y * coeffRT
       ) {
         discard;
       }
 
-      gl_FragColor = vec4(materialColor, baseColor.a);
+      gl_FragColor = baseColor;
     }
   `;
 
