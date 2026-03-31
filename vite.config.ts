c2ac5df866fe7f41168962b5ba46d60d989342dc
KANTNOLI <zahargysev02@gmail.com>
2026-03-31 20:24:23 +0300

    Release 1.0.0!!!

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,25 @@ import { resolve } from "path";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "index.ts"),
-      name: "KGEngine",
-      fileName: "kgengine",
-      formats: ["es"]
+      entry: {
+        index: resolve(__dirname, "index.ts"),
+        cameras: resolve(__dirname, "cameras.ts"),
+        engine: resolve(__dirname, "engine.ts"),
+        lighting: resolve(__dirname, "lighting.ts"),
+        objects: resolve(__dirname, "objects.ts"),
+        otherScripts: resolve(__dirname, "otherScripts.ts"),
+        playerActions: resolve(__dirname, "playerActions.ts"),
+        shaders: resolve(__dirname, "shaders.ts"),
+      },
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["three"],
       output: {
         globals: {
-          three: "THREE"
-        }
-      }
-    }
-  }
+          three: "THREE",
+        },
+      },
+    },
+  },
 });
